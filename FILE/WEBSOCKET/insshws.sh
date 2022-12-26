52df3f6cc1e4c03fec41ac8022a61ea2515f4e00
rasta-team <irfan1037.psa@gmail.com>
2022-12-26 19:33:55 +0800

    Add files via upload

--- FILE/WEBSOCKET/insshws.sh
+++ FILE/WEBSOCKET/insshws.sh
@@ -4,13 +4,15 @@ cd
 
 wget -O /usr/local/bin/ws-dropbear https://raw.githubusercontent.com/DryanZ/multi/main/FILE/WEBSOCKET/dropbear-ws.py
 wget -O /usr/local/bin/ws-stunnel https://raw.githubusercontent.com/DryanZ/multi/main/FILE/WEBSOCKET/ws-stunnel
+wget -O /usr/local/bin/ws-ovpn https://raw.githubusercontent.com/DryanZ/multi/main/FILE/OPENVPN/ws-ovpn.py
 
 chmod +x /usr/local/bin/ws-dropbear
 chmod +x /usr/local/bin/ws-stunnel
+chmod +x /usr/local/bin/ws-ovpn
 
 wget -O /etc/systemd/system/ws-dropbear.service https://raw.githubusercontent.com/DryanZ/multi/main/FILE/WEBSOCKET/service-wsdropbear && chmod +x /etc/systemd/system/ws-dropbear.service
-
 wget -O /etc/systemd/system/ws-stunnel.service https://raw.githubusercontent.com/DryanZ/multi/main/FILE/WEBSOCKET/ws-stunnel.service && chmod +x /etc/systemd/system/ws-stunnel.service
+wget -O /etc/systemd/system/ws-ovpn.service https://raw.githubusercontent.com/DryanZ/multi/main/FILE/OPENVPN/ws-ovpn.service && chmod +x /etc/systemd/system/ws-ovpn.service
 
 systemctl daemon-reload
 systemctl enable ws-dropbear.service
@@ -19,3 +21,6 @@ systemctl restart ws-dropbear.service
 systemctl enable ws-stunnel.service
 systemctl start ws-stunnel.service
 systemctl restart ws-stunnel.service
+systemctl enable ws-ovpn.service
+systemctl start ws-ovpn.service
+systemctl restart ws-ovpn.service
